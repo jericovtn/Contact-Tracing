8927d823a3330afe128ab121ce5399e8d50318ee
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-11-12 20:23:59 +0800

    Added: Dictionaries, *adjusted code for option 1*

--- Laboratory Exercise 2.py
+++ Laboratory Exercise 2.py
@@ -2,6 +2,15 @@
 # Laboratory Exercise 2: Contact Tracing
 # November 12, 2022
 
+# dictionaries
+informations = {
+
+}
+
+forSearch = {
+
+}
+
 # Display Menu
 print("\n——====== Menu ======——")
 print("  1. Add an item")
@@ -19,8 +28,16 @@ if option == 1:
     address = input("Enter your Address: ")
     age = input("Enter your age: ")
     phone = input("Enter your phone number: ")
+
+    # variables ofr dictionaries
+    value = ("Name:", name, "\nAddress:", address, "\nAge:", age, "\nPhone Number:", phone)
+    forSearch[name] = value
+
+
     print("Saved!")
-    
+    print(forSearch)
+
+
 # Option 2
 # Option 3
 
